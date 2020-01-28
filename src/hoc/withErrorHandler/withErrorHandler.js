240a5261fa11f69b90569601e19cd187c9786f11
jadon <jadonwolffs@gmail.com>
2020-01-28 15:30:14 +0200

    added hoc for handling errors

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Auxiliary/Auxiliary";
+// import axios from "../../axios-orders";
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+    clearErrorHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} modalClosed={this.clearErrorHandler}>
+            <p>Something went wrong!</p>
+            <code>{this.state.error ? this.state.error.message : null}</code>
+          </Modal>
+          <WrappedComponent {...this.props}></WrappedComponent>
+        </Aux>
+      );
+    }
+  };
+};
+export default withErrorHandler;
