f4611b0cbf449f7de875f03044d3e0ba8b4ca4a8
jadon <jadonwolffs@gmail.com>
2020-02-21 15:14:30 +0200

    added spinner and loading to orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -4,27 +4,32 @@ import axios from "../../axios-orders";
 import { connect } from "react-redux";
 import { initFetch } from "../../store/actions/index";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import Spinner from "../../components/UI/Spinner/Spinner";
 class Orders extends Component {
   componentDidMount() {
     this.props.onInit();
   }
   render() {
-    return (
-      <div>
-        {this.props.orders.map(order => (
-          <Order
-            key={order.key}
-            ingredients={order.ingredients}
-            price={order.price}
-          />
-        ))}
-      </div>
-    );
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = (
+        <div>
+          {this.props.orders.map(order => (
+            <Order
+              key={order.key}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          ))}
+        </div>
+      );
+    }
+    return orders;
   }
 }
-const mapStateToProps = state =>{
-  return {orders:state.order.orders}
-}
+const mapStateToProps = state => {
+  return { orders: state.order.orders, loading: state.order.loading };
+};
 const mapDispatchToProps = dispatch => {
   return { onInit: () => dispatch(initFetch()) };
 };
